dfba57bbe2921130842ba14bdd3e859257e3726f
acetarik <atariq.bscs21seecs@seecs.edu.pk>
2025-08-29 10:34:52 +0500

    refactor: update service images and remove unused assets for improved clarity

--- src/data/services.ts
+++ src/data/services.ts
@@ -9,32 +9,32 @@ export const Services: ServicesUnit[] = [
     {
         serviceName: "Agritech Solutions",
         serviceDescription: "Description for Agritech Solutions",
-        imageUrl: "/projects/ctd.jpg"
+        imageUrl: "/services/service-argitech.png"
     },
     {
         serviceName: "Early Warning Systems",
         serviceDescription: "Description for Early Warning Systems 2",
-        imageUrl: "/services/warn.png"
+        imageUrl: "/services/Service-early warning.png"
     },
     {
         serviceName: "Infrastructure Monitoring",
         serviceDescription: "Description for Infrastructure Monitoring",
-        imageUrl: "/services/infra.webp"
+        imageUrl: "/services/service-infrastructure.png"
     },
     {
         serviceName: "Data Analytics",
         serviceDescription: "Description for Data Analytics",
-        imageUrl: "/services/analytics.png"
+        imageUrl: "/services/service - data analytics.png"
     },
     {
         serviceName: "GIS consulting",
         serviceDescription: "Description for GIS consulting",
-            imageUrl: "/services/giscons.jpeg"
+        imageUrl: "/services/service-gis consulting.png"
     },
     {
-        serviceName: "Remote Sensing",
-        serviceDescription: "Description for Remote Sensing",
-        imageUrl: "/services/remote-sensing.jpg"
+        serviceName: "AI Automation",
+        serviceDescription: "Description for AI Automation",
+        imageUrl: "/services/service - AI automation.png"
     }
 
 ];
